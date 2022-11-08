c66d1d96ad4df6a27b6caae163e1d3fbc6c987a3
Ville Ilvonen <ville.ilvonen@unikie.com>
2022-11-08 07:09:23 +0200

    generalize to support multiple systems
    Signed-off-by: Ville Ilvonen <ville.ilvonen@unikie.com>

--- flake.nix
+++ flake.nix
@@ -1,11 +1,16 @@
 {
   description = "A very basic flake";
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        packages.hello = pkgs.hello;
+        defaultPackage = pkgs.hello;
+      });
 }
