91e4a80c36af892fece6c102e841fe9b10cd305a
Ville Ilvonen <ville.ilvonen@unikie.com>
2022-11-08 07:26:53 +0200

    rust env with nix develop
    Signed-off-by: Ville Ilvonen <ville.ilvonen@unikie.com>

--- flake.nix
+++ flake.nix
@@ -1,16 +1,20 @@
 {
-  description = "A very basic flake";
-
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, ... }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; };
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+	rustVersion = pkgs.rust-bin.stable.latest.default;
       in {
-        packages.hello = pkgs.hello;
-        defaultPackage = pkgs.hello;
-      });
+        devShell = pkgs.mkShell {
+	  buildInputs =
+	    [ (rustVersion.override { extensions = [ "rust-src" ]; }) ];
+      };
+    });
 }
